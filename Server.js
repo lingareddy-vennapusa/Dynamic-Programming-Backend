104e57ac56e1280fd905a6d9b57e531eafe2eb05
Lingareddy Vennapusa <vlingareddy14@gmail.com>
2025-12-24 23:19:41 +0530

    finish

--- Server.js
+++ Server.js
@@ -384,4 +384,8 @@ app.put(
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+  const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
